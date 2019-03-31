c19ae73076bb4262cc23d6174aaaf262af623bbe
Victoria Guevara <Guevara-chan@mail.ru>
2019-03-31 16:40:33 +0200

    Add files via upload

--- src/nimvpn.nim
+++ src/nimvpn.nim
@@ -6,23 +6,23 @@ let header = """  # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- #
              """
 #.{ [Procs]
 proc main(country: string): string =
-    styledEcho fgGreen, styleBright, header.replace("  ", "")
+    fgGreen.styledEcho styleBright, header.replace("  ", "")
     # Parsing OpenVPN location.
     let config  = getTempDir().joinPath "vpnconfig.tmp"
     let exe     = try: "HKEY_LOCAL_MACHINE\\SOFTWARE\\OpenVPN".open(samRead).readString("exe_path") except: ""
     if exe == "": return "[nimvpn] FAULT:: unable to locate OpenVPN ! "
     let command = fmt"{exe} --connect-retry-max 2 --connect-timeout 10 --config {config}"
     # Parsing server list.
-    styledEcho fgYellow, styleBright, "[nimvpn] getting server list..."
-    let list = newHttpClient().getContent("http://www.vpngate.net/api/iphone/").splitLines[2..^1]
-    styledEcho fgMagenta, styleBright, fmt"[nimvpn] looking for VPNs from {country}:"
+    fgYellow.styledEcho styleBright, "[nimvpn] getting server list..."
+    let list = newHttpClient().getContent("http://www.vpngate.net/api/iphone/").splitLines[2..^3]
+    fgMagenta.styledEcho styleBright, fmt"[nimvpn] looking for VPNs from {country}:"
     # Main parsing loop.
-    for entry in list:
+    for idx, entry in list:
         let chunks = entry.split ','
-        if chunks.len < 15: break
+        if chunks.len != 15: fgRed.styledEcho(fmt"[nimvpn] invalid entry encountered."); continue
         if country != chunks[6]: continue
         config.writeFile chunks[14].decode()
-        styledEcho fgCyan, styleBright, fmt"[nimvpn] running OpenVPN for {chunks[1]}."
+        fgCyan.styledEcho styleBright, fmt"[nimvpn] running OpenVPN for {chunks[1]} [{idx}/{list.len}]"
         try:
             let openvpn = command.startCmd
             defer: openvpn.terminate()
@@ -40,4 +40,3 @@ proc main(country: string): string =
 # -Main code-
 stdout.styledWrite fgRed, styleBright, main(if paramCount() > 1: paramStr(1) else: "US")
 3000.sleep()
-quit 0
